02444c8debecb82480cde115891b340d8674a3f3
yasirIrshad <yirshad900@gmail.com>
2022-11-25 22:55:41 +0500

    Changes

--- src/pages/protected/home/Home.js
+++ src/pages/protected/home/Home.js
@@ -1,23 +1,25 @@
-import React, { useLayoutEffect, useRef ,useState , useEffect} from "react";
-import { Dimensions, View , Text , StyleSheet } from "react-native";
+import React, { useLayoutEffect, useRef, useState, useEffect } from "react";
+import { Dimensions, View, Text, StyleSheet } from "react-native";
 import { CText, ProgressiveImage } from "../../../uiComponents";
 import Swiper from "react-native-swiper";
 import Styles from "./Home.style";
 import { useSelector, useDispatch } from "react-redux";
 import { Header } from "../../../containers";
+import VideoPlayer from '../../../containers/VideoPlayer'
+// import VideoPlayer from 'react-native-video-controls';
+
 import { getTrending } from "../../../store/actions/Root.action";
 import Video from 'react-native-video';
 
 // import VideoPlayer from "react-native-video-player";
-import VideoPlayer from 'react-native-video-controls';
 const { width, height } = Dimensions.get("window");
-import ApiSauce  from "../../../utils/network"
-import {GET_TRENDING, TRENDING} from "../../../config/webservices"
+import ApiSauce from "../../../utils/network"
+import { GET_TRENDING, TRENDING } from "../../../config/webservices"
 function Home(props) {
 
- const [data, setData] = useState()
+    const [data, setData] = useState()
     console.log("🚀 ~ file: Home.js ~ line 16 ~ Home ~ data", data)
-    
+
     const dispatch = useDispatch();
     useLayoutEffect(() => {
         handleTrending();
@@ -29,46 +31,53 @@ function Home(props) {
 
     const slider = useRef();
 
-    const [video , setVideo] = useState([
-        {uri:'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4'},
-        {uri:'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4'},
-        {uri:'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4'},
-        {uri:'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4'},
-        {uri:'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4'},
-        {uri:'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4'},
-        {uri:'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4'},
-        {uri:'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4'},
+    const [video, setVideo] = useState([
+        { uri: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4' },
+        { uri: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4' },
+        { uri: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4' },
+        { uri: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4' },
+        { uri: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4' },
+        { uri: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4' },
+        { uri: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4' },
+        { uri: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4' },
     ])
-    useEffect(()=>{
+    useEffect(() => {
         handleApi()
-    },[])
+    }, [])
     // , '24|p52iuFDApHZCYPuy0MmjFp7igWQbWMq2RmtrYbby'
-    const handleApi = async() => {
-        try{
+    const handleApi = async () => {
+        try {
             const trendData = await ApiSauce.getWithoutToken(GET_TRENDING)
             setData(trendData.data.trending)
             console.log("🚀 ~ file: Home.js ~ line 45 ~ handleApi ~ trendData", trendData)
 
         }
-        catch(err){
+        catch (err) {
             console.log("🚀 ~ file: Home.js ~ line 46 ~ handleApi ~ err", err)
         }
     }
-   
+
 
     const renderSlides = () => {
-        if(data?.length > 0 ){
-            return(
+        if (data?.length > 0) {
+            return (
                 data?.map((val, index) => {
                     console.log("🚀 ~ file: Home.js ~ line 59 ~ returndata?.map ~ val", val)
                     return (
-                        <Video source={{uri:val }}   // Can be a URL or a local file.
-    //    ref={(ref) => {
-    //      this.player = ref
-    //    }}                                      // Store reference
-    //    onBuffer={this.onBuffer}                // Callback when remote video is buffering
-    //    onError={this.videoError}               // Callback when video cannot be loaded
-       style={styles.backgroundVideo} />
+                        <VideoPlayer
+source={{uri: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4'}}
+/>
+                        // <Video source={{ uri: 'https://ayhman.webappcart.com/storage/trendingVideos/jpw8exb5afyvqtc641h3irzgms2k0d9lun7.mp4' }}   // Can be a URL or a local file.
+                        //     //    ref={(ref) => {
+                        //     //      this.player = ref
+                        //     //    }}                                      // Store reference
+                        //     //    onBuffer={this.onBuffer}                // Callback when remote video is buffering
+                        //     //    onError={this.videoError}               // Callback when video cannot be loaded
+                        //     style={styles.backgroundVideo} 
+                        //     fullscreen={true}
+                        //     fullscreenAutorotate='portrait'
+                        //     posterResizeMode='cover'
+                        //     resizeMode="cover" />
                         // <VideoPlayer
                         // source={{uri: 'https://vjs.zencdn.net/v/oceans.mp4'}}
                         //     // thumbnail={{ uri: 'https://img.freepik.com/free-photo/full-length-image-smiling-african-woman-leather-jacket_171337-14041.jpg' }}
@@ -77,19 +86,19 @@ function Home(props) {
                         //     disableFullscreen='false'
                         //     resizeMode="cover"
                         // />
-                  
-                        );
+
+                    );
                 })
             )
-        }else{
-            return(
-                <View style={{justifyContent:'center', alignItems:'center'}}>
+        } else {
+            return (
+                <View style={{ justifyContent: 'center', alignItems: 'center' }}>
                     <Text>No Data</Text>
                 </View>
             )
         }
-        
-             
+
+
     };
 
     const headerProps = {
@@ -107,18 +116,21 @@ function Home(props) {
     };
 
     return (
-        <View style={{  flex: 1 }}>
+        <View style={{ flex: 1 }}>
 
             <Header {...headerProps} />
             <Swiper
                 ref={slider}
-                style={{ backgroundColor: "transparent" }}
+                style={{ backgroundColor: "transparent"  , height:'95%'}}
                 showsButtons={false}
                 showsPagination={false}
                 horizontal={false}
                 loop={false}
             >
-                {renderSlides()}
+                             <VideoPlayer
+source={{uri: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4'}}
+/>
+                {/* {renderSlides()} */}
             </Swiper>
         </View>
     );
@@ -126,10 +138,11 @@ function Home(props) {
 export default Home;
 var styles = StyleSheet.create({
     backgroundVideo: {
-      position: 'absolute',
-      top: 0,
-      left: 0,
-      bottom: 0,
-      right: 0,
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        bottom: 0,
+        right: 0,
+        flex:1,
     },
-  });
+});
